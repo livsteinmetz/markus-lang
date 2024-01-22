584d800968c8cec260c3a1d06ef19c2fbc1c9229
livsteinmetz <olivia.r.steinmetz@gmail.com>
2024-01-22 16:50:25 -0500

    update bio

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,38 +5,67 @@ function Info() {
     <section>
       <h2>About</h2>
       <p>
-        Markus Lang is a bassist from Allentown, New Jersey. He is currently
-        pursuing a Post-Baccalaureate Diploma at the Curtis Institute of
-        Music; studying with Hal Robinson, and Edgar Meyer; and will
-        graduate this Spring. As an orchestral musician, he has performed as
-        principal bass with the Gothenburg Symphony Orchestra, and the New
-        Jersey Symphony Orchestra, and as a sub list member with the
-        Philadelphia Orchestra. Markus also plays with the Camden-based
-        orchestra, Symphony in C, where he holds a section position. He
-        graduated from the Juilliard School in 2020, where he studied with
-        Albert Laszlo, Joseph Conyers, and Rex Surany.
+        Since 2022, Markus Lang has held the position of Principal Bass at the{" "}
+       Gothenburg Symphony Orchestra under
+        the baton of Santtu-Matias Rouvali. His musical journey extends beyond
+        this role, with invitations to perform as Guest Principal with orchestras
+        such as the Royal Stockholm
+        Philharmonic and the Oslo Philharmonic.
+        Additionally, Markus has served as a substitute list member with the
+        Philadelphia Orchestra since 2019.
       </p>
       <p>
-        Passionate about historical double bass playing, Markus has
-        performed extensively with Juilliard 415; the school’s early music
-        ensemble. He toured with the group in New Zealand and Scandinavia,
-        performing Baroque music on historical instruments. He studied with
-        Doug Balliett; professor of baroque bass and violone at the
-        Juilliard School.
+        Beyond his orchestral engagements, Markus is deeply passionate about
+        historical double bass playing. He has collaborated extensively with
+        Juilliard 415, the School's primary period-instrument ensemble, touring
+        New Zealand and Scandinavia to bring Baroque music to life on historical
+        instruments. The ensemble worked with resident and guest artists such as
+        Monica Huggett, Nicholas McGegan, Ton Koopman, William Christie,
+        Masaaki Suzuki, Jordi Savall, Laurence Equilbey, Harry Bicket, and many
+        others, performing a dynamic programming of repertoire from the late
+        16th through the early 19th centuries on period instruments. Markus
+        delved into the art of baroque bass and violone with the guidance of Doug
+        Balliett at the Juilliard School.
       </p>
       <p>
-        Equally interested in contemporary music, Markus focuses on music
-        that highlights the many colors the bass has to offer. He spent two
-        consecutive summers at the Lucerne Festival Academy; under the baton
-        of Riccardo Chailly, he was invited to be a member of the Academy’s
-        2019 Alumni Orchestra. He played regularly with Juilliard’s two
-        contemporary ensembles, AXIOM, and the New Juilliard Ensemble.
-        During the COVID-19 pandemic, Markus was part of an online
-        initiative, the Berio Sequenza ZOOMarathon, where he performed the
-        Sequenza for Double Bass on live internet broadcast along with 13
-        other musicians. He has spent the past several summers performing at
-        the Verbier Festival, Music Academy of the West, Domaine Forget, and
-        the Wabass Institute.
+        Markus's versatility extends to the realm of contemporary music, where
+        he explores compositions that showcase the diverse colors and
+        capabilities of the double bass. Back at Juilliard, he regularly
+        collaborated with contemporary ensembles, AXIOM, and the New Juilliard
+        Ensemble. His dedication to contemporary sounds also led him to spend
+        two consecutive summers at the Lucerne Festival Academy. Under the baton
+        of Riccardo Chailly, Markus has returned to the Academy’s Alumni
+        Orchestra. During the COVID-19 pandemic, Markus participated in the Berio
+        Sequenza ZOOMarathon, an online initiative where he performed the
+        Sequenza for Double Bass on live internet broadcast alongside 13 other
+        musicians. In Gothenburg, he's fortunate to work with
+        Barbara Hannigan on new music and innovative
+        programming.
+      </p>
+      <p>
+        Markus just recently released a performance of Kaija Saariaho's work for
+        Double Bass and Electronics, "Folia," on <a href="https://www.gso.se/gsoplay/">GSOPlay</a>,{" "}
+        to honor the late composer. Looking ahead, in the Fall of 2024, he will
+        perform Tan Dun's "Wolf Totem" Double Bass Concerto with the
+        Gothenburg Symphony Orchestra.
+      </p>
+      <p>
+        Markus Lang, born into a musical family with an American horn player
+        father and Finnish cellist mother, is grateful for the rich musical
+        upbringing that took him between the United States and Scandinavia. He
+        first found his own passion for music playing electric bass in his
+        childhood home in New Jersey. Markus embarked on his double bass journey
+        in Philadelphia, where he studied with Mary Javian and Ranaan Meyer. A
+        graduate of both the Juilliard
+        School and the Curtis Institute of
+        Music, Markus studied with Hal Robinson, Albert Laszlo, Joseph
+        Conyers, Edgar Meyer, and Rex Surany. He also dedicated several summers
+        to prestigious festivals, including the Verbier Festival,{" "}
+        Music Academy of the West,{" "}
+        Domaine Forget, and the{" "}
+        Wabass Institute. Markus has the privilege to play on a
+        rare English double bass by George Panormo, generously loaned to him by
+        the Gothenburg Symphony Orchestra.
       </p>
     </section>
   );
